bd1aa3f038496e2e224ad82f8056093e7940bbc3
Imprudent Friend <ImprudentFriend@gmail.com>
2021-02-25 17:26:35 -0500

    fixed db connection issues

--- database/connection.js
+++ database/connection.js
@@ -2,13 +2,17 @@ const mysql = require("mysql");
 const util = require("util");
 
 const con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "CleanSlate68@",
-  database: "employees"
-});
+    host: "localhost",
+    user: "root",
+    password: "CleanSlate68@",
+    database: "employees"
+  });
+  
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+  });
 
-con.connect();
 con.query = util.promisify(con.query);
 
 module.exports = con;
